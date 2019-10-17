861c612566b0252a105763eaa85ac06918158819
nfrerebeau <33983845+nfrerebeau@users.noreply.github.com>
2019-10-17 14:03:41 +0200

    Update package description

---
+++ R/archeosciences2018-package.R
@@ -0,0 +1,35 @@
+#' @details
+#' \tabular{ll}{
+#'  \strong{Package:} \tab archeosciences2018 \cr
+#'  \strong{Type:} \tab Compendium \cr
+#'  \strong{Version:} \tab 1.0 \cr
+#'  \strong{License:} \tab GPL-3 \cr
+#'  \strong{DOI:} \tab \href{https://doi.org/xxx}{xxx}
+#' }
+#' @author
+#' \strong{Full list of authors and contributors} (alphabetic order)
+#'
+#' \tabular{ll}{
+#'  Nicolas Frerebeau \tab \emph{Université Bordeaux Montaigne, France}
+#'  Michel Pernot \tab \emph{CNRS, France}
+#' }
+#'
+#' \strong{Package maintainer}
+#'
+#' Nicolas Frerebeau\cr
+#' \email{nicolas.frerebeau@@u-bordeaux-montaigne.fr}
+#'
+#' IRAMAT-CRP2A (UMR 5060)\cr
+#' Maison de l'Archéologie\cr
+#' Université Bordeaux Montaigne\cr
+#' F-33607 Pessac cedex\cr
+#' France
+#' @note
+#' This study received a state financial support managed by the Agence Nationale
+#' de la Recherche (France) throught the program \emph{Investissements d'avenir}
+#' (ref. ANR-10-LABX-52).
+#' @name archeosciences2018-package
+#' @aliases archeosciences2018-package archeosciences2018
+#' @docType package
+#' @keywords internal
+"_PACKAGE"
